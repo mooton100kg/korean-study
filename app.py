e09acc3711d78123df9b281a4c88e188d50ffef3
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 03:25:35 +0700

    gfd

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-        
+        TextSendMessage(text=event))
+
 if __name__ == "__main__":
     app.run(debug=False)
