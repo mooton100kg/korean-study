5a33270579a9c6603f7d44c51f1dcb9724ffac5b
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 03:13:09 +0700

    gtf

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-import os
-import sys
-from argparse import ArgumentParser
-
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -29,18 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+line_bot_api = LineBotApi('A/LutxhuQ5rBx5XTmfo5CyEZA4ov5ijZ7L7KuVzhLEHEVYIvnJV8+XAbvnUtjWfvu60ilWdfR6abusJkuLBNA4TDOGBN1OnSK+qkMTZf8ZzGi3m0oBy5kOzOS6CTc8fZ5mTUULWfWuP39dYo6NynqQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('65cdcfc23c498613eda60112b5de9421')
 
 
 @app.route("/callback", methods=['POST'])
@@ -56,25 +29,18 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
+def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run()
