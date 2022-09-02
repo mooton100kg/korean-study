2e83ed3b59fc67dabcd37d083c2eb197128ea49e
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 03:20:31 +0700

    gtf

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
+
+line_bot_api = LineBotApi('A/LutxhuQ5rBx5XTmfo5CyEZA4ov5ijZ7L7KuVzhLEHEVYIvnJV8+XAbvnUtjWfvu60ilWdfR6abusJkuLBNA4TDOGBN1OnSK+qkMTZf8ZzGi3m0oBy5kOzOS6CTc8fZ5mTUULWfWuP39dYo6NynqQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('65cdcfc23c498613eda60112b5de9421')
 
 data = [
         {
@@ -24,7 +37,24 @@ data = [
 def hello():
     return "Hello Flask-Heroku"
 
+@app.route('/callback', methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+    
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
