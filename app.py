0f1cfe1450112b40ba4a713867f27c334f37ed90
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 04:54:26 +0700

    gf

--- app.py
+++ app.py
@@ -25,29 +25,29 @@ handler = WebhookHandler(CHANNEL_SECRET)
 def hello():
     return "Hello Flask-Heroku"
 
-@app.route('/callback', methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+# @app.route('/callback', methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
 
-    return 'OK'
+#     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
 
 @app.route('/richmenu', methods=['POST'])
 def richmenu():
