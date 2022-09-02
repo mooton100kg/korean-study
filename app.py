34acc6c1d93f26cf2fff5236be7f34dc31f90526
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 04:31:37 +0700

    grfe

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # from flask import Flask, request, abort
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 # from linebot.exceptions import (
 #     InvalidSignatureError
 # )
