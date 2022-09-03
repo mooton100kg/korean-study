42dca12f00434d3794a5c245c1d0b422f346d2a3
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-03 14:25:19 +0700

    gfd

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
     flex = json.loads(flexmessage())
     line_bot_api.reply_message(
         event.reply_token, FlexSendMessage(alt_text="flex", contents=flex)
