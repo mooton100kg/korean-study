6ec20e2d23a340a5d2d5d757f7af571b7ca18c92
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-04 00:54:15 +0700

    gfd

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_messaßge(
+    line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=str(event)))
 
