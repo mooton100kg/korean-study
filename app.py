199dd2854c60888a0cafb9bc24dcb5cabbcb7c11
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-04 05:18:34 +0700

    gfd

--- app.py
+++ app.py
@@ -135,14 +135,14 @@ def handle_event(event):
 
         
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+    # arg_parser = ArgumentParser(
+    #     usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    # )
+    # arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    # arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    # options = arg_parser.parse_args()
 
-    app.run(debug=options.debug, port=options.port)
-
+    # app.run(debug=options.debug, port=options.port)
+    app.run(debug=False)
 
 
