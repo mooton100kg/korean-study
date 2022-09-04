823d21767497e31ebf944e3192da5266a6bc7e87
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-09-04 13:17:12 +0700

    gf

--- Constants/spreadsheet.py
+++ Constants/spreadsheet.py
@@ -9,10 +9,10 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('jsonfile/alert-brook-361418-d95992f17aaf.json', scope)
 
 # authorize the clientsheet 
-client = gspread.authorize(creds)
+# client = gspread.authorize(creds)
 
-sheet = client.open('Korean')
-sheet_instance = sheet.get_worksheet(0)
+# sheet = client.open('Korean')
+# sheet_instance = sheet.get_worksheet(0)
 
 def getData():
     # records_data = sheet_instance.get_all_records()
@@ -23,19 +23,19 @@ def getData():
     
     return "hello"
 
-def update_data(word, correct, type):
-    cell = sheet_instance.find(word)
+# def update_data(word, correct, type):
+#     cell = sheet_instance.find(word)
 
-    if type == "flashcard":
-        col = 3
-    elif type == "spelling":
-        col = 4
+#     if type == "flashcard":
+#         col = 3
+#     elif type == "spelling":
+#         col = 4
 
-    if correct:
-        x = int(sheet_instance.cell(cell.row, col).value) + 1
-    elif not correct:
-        x = int(sheet_instance.cell(cell.row, col).value) - 1
-    sheet_instance.update_cell(cell.row, col, x)
+#     if correct:
+#         x = int(sheet_instance.cell(cell.row, col).value) + 1
+#     elif not correct:
+#         x = int(sheet_instance.cell(cell.row, col).value) - 1
+#     sheet_instance.update_cell(cell.row, col, x)
 
-def add_word(korean, thai):
-    sheet_instance.insert_row([korean, thai, 0, 0], index=2)
+# def add_word(korean, thai):
+#     sheet_instance.insert_row([korean, thai, 0, 0], index=2)
